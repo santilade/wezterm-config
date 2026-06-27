4ef750e37b855b3ae708b7ba52067ea9bee21fb9
santilade <santilade@proton.me>
2026-06-28 00:40:48 +0200

    wsl

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,19 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+local is_windows = wezterm.target_triple:find("windows") ~= nil
+
+if is_windows then
+	config.default_domain = "WSL:Ubuntu"
+	config.wsl_domains = {
+		{
+			name = "WSL:Ubuntu",
+			distribution = "Ubuntu",
+			default_cwd = "~",
+		},
+	}
+end
+
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.85
 config.window_padding = {
